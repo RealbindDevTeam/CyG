51302317bfcf90b8fc9f0f9238c6771a315563aa
leonardoespinosa <leonardoespinosa@realbind.com>
2018-04-03 17:00:36 -0500

    Add button to download establishment qr code

--- cyg_web/client/imports/app/web/administrator/administration/establishment/establishment/establishment.component.ts
+++ cyg_web/client/imports/app/web/administrator/administration/establishment/establishment/establishment.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { Router } from "@angular/router";
 import { MeteorObservable } from 'meteor-rxjs';
 import { TranslateService } from '@ngx-translate/core';
-import { MatDialogRef, MatDialog } from '@angular/material';
+import { MatDialogRef, MatDialog, MatSnackBar } from '@angular/material';
 import { Meteor } from 'meteor/meteor';
 import { UserLanguageService } from '../../../../services/general/user-language.service';
 import { Establishment } from '../../../../../../../../both/models/establishment/establishment.model';
@@ -13,6 +13,12 @@ import { Country } from '../../../../../../../../both/models/general/country.mod
 import { Countries } from '../../../../../../../../both/collections/general/country.collection';
 import { EstablishmentPoint } from '../../../../../../../../both/models/points/establishment-point.model';
 import { EstablishmentPoints } from '../../../../../../../../both/collections/points/establishment-points.collection';
+import { Parameters } from '../../../../../../../../both/collections/general/parameter.collection';
+import { Parameter } from '../../../../../../../../both/models/general/parameter.model';
+import { EstablishmentQR } from '../../../../../../../../both/models/establishment/establishment-qr.model';
+import { EstablishmentQRs } from '../../../../../../../../both/collections/establishment/establishment-qr.collection';
+
+let jsPDF = require('jspdf');
 
 @Component({
     selector: 'establishment',
@@ -25,8 +31,10 @@ export class EstablishmentComponent implements OnInit, OnDestroy {
     private establishments: Observable<Establishment[]>;
 
     private establishmentSub: Subscription;
+    private _establishmentQRSub: Subscription;
     private countriesSub: Subscription;
     private _establishmentPointsSub: Subscription;
+    private parameterSub: Subscription;
     private _ngUnsubscribe: Subject<void> = new Subject<void>();
 
     public _dialogRef: MatDialogRef<any>;
@@ -40,13 +48,15 @@ export class EstablishmentComponent implements OnInit, OnDestroy {
      * @param {MatDialog} _dialog
      * @param {NgZone} _ngZone
      * @param {UserLanguageService} _userLanguageService
+     * @param {MatSnackBar} _snackBar
      */
     constructor(private router: Router,
         private _formBuilder: FormBuilder,
         private translate: TranslateService,
         public _dialog: MatDialog,
         private _ngZone: NgZone,
-        private _userLanguageService: UserLanguageService) {
+        private _userLanguageService: UserLanguageService,
+        private _snackBar: MatSnackBar) {
         translate.use(this._userLanguageService.getLanguage(Meteor.user()));
         translate.setDefaultLang('en');
     }
@@ -68,7 +78,9 @@ export class EstablishmentComponent implements OnInit, OnDestroy {
                 this._establishmentPointsSub = MeteorObservable.subscribe('getEstablishmentPointsByIds', _establishmentIds).takeUntil(this._ngUnsubscribe).subscribe();
             });
         });
+        this._establishmentQRSub = MeteorObservable.subscribe('getEstablishmentQRsByAdmin', this._user).takeUntil(this._ngUnsubscribe).subscribe();
         this.countriesSub = MeteorObservable.subscribe('countries').takeUntil(this._ngUnsubscribe).subscribe();
+        this.parameterSub = MeteorObservable.subscribe('getParameters').takeUntil(this._ngUnsubscribe).subscribe();
     }
 
     /**
@@ -123,6 +135,59 @@ export class EstablishmentComponent implements OnInit, OnDestroy {
         } else {
             return 0;
         }
+    }
+
+    /**
+     * Function to print establishment QR code
+     * @param {string} _pEstablishmentName 
+     */
+    printQrPdf(_pEstablishmentId: string, _pEstablishmentName: string): void {
+        let file_name = this.itemNameTraduction('RESTAURANT.QR_CODE');
+        let iurest_url: string = Parameters.findOne({ name: 'iurest_url_short' }).value;
+        let _lEstablishmentQR: EstablishmentQR = EstablishmentQRs.findOne({ establishment_id: _pEstablishmentId });
+        let _establishmentName: string[] = [];
+
+        if (_pEstablishmentName.length <= 28) {
+            _establishmentName.push(_pEstablishmentName);
+        } else {
+            _establishmentName.push(_pEstablishmentName.substr(0, 28));
+            _establishmentName.push(_pEstablishmentName.substr(28, 50));
+        }
+
+        if (_lEstablishmentQR) {
+            let qr_pdf = new jsPDF("portrait", "mm", "a4");
+            qr_pdf.setFontSize(16);
+            qr_pdf.rect(55, 25, 90, 90);
+            qr_pdf.text(60, 33, _establishmentName);
+            if (_pEstablishmentName.length <= 28) {
+                qr_pdf.addImage(_lEstablishmentQR.QR_URI, 'JPEG', 70, 37, 60, 60);
+                qr_pdf.setFontSize(13);
+                qr_pdf.text(77, 102, iurest_url);
+            } else {
+                qr_pdf.addImage(_lEstablishmentQR.QR_URI, 'JPEG', 70, 44, 60, 60);
+                qr_pdf.setFontSize(13);
+                qr_pdf.text(77, 109, iurest_url);
+            }
+            qr_pdf.output('save', _pEstablishmentName.substr(0, 15) + '_' + file_name + '.pdf');
+
+            let _lMessage: string = this.itemNameTraduction('RESTAURANT.QR_CODE_DOWNLOADED');
+            this._snackBar.open(_lMessage, '', { duration: 2500 });
+        } else {
+            let _lMessage: string = this.itemNameTraduction('RESTAURANT.QR_CODE_ERROR');
+            this._snackBar.open(_lMessage, '', { duration: 2500 });
+        }
+    }
+
+    /**
+     * Function to translate information
+     * @param {string} _itemName
+     */
+    itemNameTraduction(_itemName: string): string {
+        var _wordTraduced: string;
+        this.translate.get(_itemName).subscribe((res: string) => {
+            _wordTraduced = res;
+        });
+        return _wordTraduced;
     }
 
     /**
--- cyg_web/client/imports/app/web/administrator/administration/establishment/register/establishment-register.component.ts
+++ cyg_web/client/imports/app/web/administrator/administration/establishment/register/establishment-register.component.ts
@@ -131,7 +131,7 @@ export class EstablishmentRegisterComponent implements OnInit, OnDestroy {
         this._establishmentForm = new FormGroup({
             country: new FormControl('', [Validators.required]),
             city: new FormControl('', [Validators.required]),
-            name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(70)]),
+            name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(50)]),
             address: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(90)]),
             phone: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(30)]),
             pointsValidity: new FormControl('', [Validators.required]),
